d1a26e52b20593a0635c7253f742e43ca0168ad1
Blue Onion <rawatadityasingh963@gmail.com>
2025-04-03 22:11:44 +0530

    Little optimization in auth

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,21 +1,58 @@
 import {
-
-    SignInButton,
-    SignUpButton,
-    SignedIn,
-    SignedOut,
-    UserButton,
-  } from "@clerk/nextjs";
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { LayoutDashboard, PenBox } from "lucide-react";
 const Header = () => {
   return (
-    <div className="fixed top-0">
-      <SignedOut>
-        <SignInButton />
-        <SignUpButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+    <div className="fixed top-0 z-50 w-full bg-white/80 backdrop-blur-md border-b">
+      <nav className="flex items-center justify-between p-4 container max-w-full mx-auto">
+        <Link href={"/"}>
+          <Image
+            src={"/logo.png"}
+            height={60}
+            width={200}
+            alt="Logo"
+            className="h-12 w-auto object-contain"
+          />
+        </Link>
+        <div className="">
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/dashboard">
+              <Button variant={"outline"}>Login</Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <div className="flex justify-between items-center gap-4">
+
+            <Link href={"/dashboard"} >
+            <Button className="text-gray-600 hover:text-blue-600 items-center flex gap-2" variant={"outline"}>
+              <LayoutDashboard size={18}/>
+              <span className="hidden md:inline">DashBoard</span>
+            </Button>
+            </Link>
+            <Link href={"/transaction/create"} >
+            <Button className=" items-center flex gap-2" >
+              <PenBox size={18}/>
+              <span className="hidden md:inline">Add Transaction</span>
+            </Button>
+            </Link>
+            <UserButton appearance={{
+              elements:{
+
+                avatarBox:"w-10 h-10"
+              }
+            }}/>
+            </div>
+          </SignedIn>
+        </div>
+      </nav>
     </div>
   );
 };
