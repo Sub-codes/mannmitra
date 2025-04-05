4e6cd109dec0f3395246cb4144ba5a30b8b156c7
Blue Onion <rawatadityasingh963@gmail.com>
2025-04-05 14:37:10 +0530

    Hero Category Updating

--- app/layout.js
+++ app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
       <html lang="en">
         <body className={`${inter.className}`}>
           <Header />
-          <main className="min-h-screen mt-20">{children}</main>
+          <main className="min-h-screen mt-40">{children}</main>
           <footer className="bg-blue-100 py-12">
             <div className="mx-auto container text-center px-4 text-gray-600">
               © {new Date().getFullYear()} Blue Onion. All rights reserved.
