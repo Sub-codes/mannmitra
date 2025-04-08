a0a9e7a54fcf913b82fc5f931c1ea933a76f1a2c
Blue Onion <rawatadityasingh963@gmail.com>
2025-04-08 20:35:58 +0530

    Table cerated

--- app/(main)/account/[id]/page.jsx
+++ app/(main)/account/[id]/page.jsx
@@ -1,7 +1,9 @@
 import { getAccountWithTransaction } from '@/actions/account';
 import NotFound from '@/app/not-found';
-import { parse } from 'date-fns';
+import { Suspense } from 'react';
 import React from 'react'
+import TransactionTable from '../_components/TransactionTable';
+import { BarLoader } from 'react-spinners';
 
 const page =async({params}) => {
     const {id}=await params;
@@ -15,7 +17,9 @@ const page =async({params}) => {
     
   return (
 
-      <div className="space-y-8 flex px-5 gap-4 items-end justify-between">
+      <div className="space-y-8 ">
+        <div className="flex px-5 gap-4 items-end justify-between">
+
         <div className="">
             <h1 className="text-5xl sm:text-6xl font-bold gradient-title capitalize">{account.name}</h1>
             <p className="text-muted-foreground">{account.type.charAt(0) + account.type.slice(1).toLowerCase()}</p>
@@ -27,6 +31,12 @@ const page =async({params}) => {
         {/* Chart Section */}
         {/* Table Section */}
       </div>
+        <Suspense fallback={<BarLoader className='mt-4' width={"100%"} color='#9333ea'/>}>
+
+        <TransactionTable transactions={transaction}/>
+        </Suspense>
+        </div>
+
 
   )
 }
