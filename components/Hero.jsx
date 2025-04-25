6fb7035a3f3b5a4f1193037e77bce1a7a50765c6
Blue Onion <rawatadityasingh963@gmail.com>
2025-04-25 18:20:01 +0530

    Link Update

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -17,24 +17,21 @@ const HeroSection = () => {
         imageElement.classList.remove("scrolled");
       }
     };
-    window.addEventListener("scroll",handleScroll)
-    return ()=>window.removeEventListener("scroll",handleScroll)
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
     <div className="pb-20 px-4">
       <div className="mx-auto container text-center">
         <h1 className="text-5xl md:text-8xl gradient-title lg:text-[105px] pb-6 font-bold">
-        Heal Your Mind <br/>
-With Intelligence
-
-
+          Heal Your Mind <br />
+          With Intelligence
         </h1>
         <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
-        An empathetic AI companion that listens, supports, and guides you through tough times. Chat freely, track your emotions, and book appointments with real mental health professionals — all in one place.
-
-
-
+          An empathetic AI companion that listens, supports, and guides you
+          through tough times. Chat freely, track your emotions, and book
+          appointments with real mental health professionals — all in one place.
         </p>
         <div className="flex justify-center gap-4">
           <Link href={"/chat-bot"}>
@@ -42,14 +39,9 @@ With Intelligence
               Start Chatting
             </Button>
           </Link>
-          <Link
-            href={
-              "/book-appointment"
-            }
-          >
-            
+          <Link href={"https://github.com/Blue-Onion/mannmitra"}>
             <Button className={"px-8"} variant={"outline"} size={"lg"}>
-              Learn More
+              Learn
             </Button>
           </Link>
         </div>
