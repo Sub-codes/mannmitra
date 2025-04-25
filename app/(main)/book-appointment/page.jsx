687161f3c838aea170760467299e9c9229a06221
Blue Onion <rawatadityasingh963@gmail.com>
2025-04-25 19:11:16 +0530

    Page Protection

---
+++ app/(main)/book-appointment/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const bookAppointment = () => {
+  return (
+    <div>
+      Book an Appointment
+    </div>
+  )
+}
+
+export default bookAppointment
---
+++ app/(main)/chat-bot/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Chat Bot
+    </div>
+  )
+}
+
+export default page
